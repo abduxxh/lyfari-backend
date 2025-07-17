8af20ad6fb5586a37b41d1e96028dd241b8db38d
Lyfari <soulmatch.lyfari@gmail.com>
2025-07-17 18:49:36 +0530

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,6 +1,5 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
-    const { name, email, q1, q2 } = req.body;
     console.log("New Submission:", { name, email, q1, q2 });
     res.status(200).json({ message: "Submission successful" });
   } else {
