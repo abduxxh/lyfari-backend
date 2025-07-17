f1fc460879c04f0783ee97cf1b8acd26c1c6a9f0
Lyfari <soulmatch.lyfari@gmail.com>
2025-07-17 18:56:31 +0530

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,9 +1,9 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
+export default function handler(req, res) {
+  if (req.method === "POST") {
     const { name, email, q1, q2 } = req.body;
-    console.log('Received:', { name, email, q1, q2 });
-    res.status(200).json({ message: 'Data received successfully' });
+    console.log("New Submission:", { name, email, q1, q2 });
+    res.status(200).json({ message: "Submission successful" });
   } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+    res.status(405).json({ message: "Method Not Allowed" });
   }
 }
