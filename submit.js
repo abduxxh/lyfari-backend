95698d93b6cadee5faee06e1bde89c6d285b1dd6
Lyfari <soulmatch.lyfari@gmail.com>
2025-07-17 18:56:00 +0530

    Delete submit.js

--- submit.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    console.log("New Submission:", { name, email, q1, q2 });
-    res.status(200).json({ message: "Submission successful" });
-  } else {
-    res.status(405).json({ message: "Method Not Allowed" });
-  }
-}
